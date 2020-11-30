70bab3ffa82a8d5f1f6a7c58833e99de8a85b710
joaoh1 <joaoh1@users.noreply.github.com>
2020-11-30 18:18:21 -0300

    1.16.4, fix incompatibility with OptiFine?

--- src/main/java/io/github/joaoh1/boringtweaks/mixin/DebugHudMixin.java
+++ src/main/java/io/github/joaoh1/boringtweaks/mixin/DebugHudMixin.java
@@ -1,8 +1,14 @@
 package io.github.joaoh1.boringtweaks.mixin;
 
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import io.github.joaoh1.boringtweaks.config.BoringTweaksConfigPojo;
 import net.minecraft.client.font.TextRenderer;
@@ -11,7 +17,11 @@ import net.minecraft.client.util.math.MatrixStack;
 
 @Mixin(DebugHud.class)
 public class DebugHudMixin {
-    @Redirect(
+    @Shadow
+    @Final
+    private TextRenderer fontRenderer;
+
+    @Inject(
         at = @At(
             value = "INVOKE",
             target = "net/minecraft/client/font/TextRenderer.draw(Lnet/minecraft/client/util/math/MatrixStack;Ljava/lang/String;FFI)I"
@@ -19,13 +29,18 @@ public class DebugHudMixin {
         method = {
             "renderLeftText",
             "renderRightText"
-        }
+        },
+        locals = LocalCapture.CAPTURE_FAILHARD,
+        cancellable = true
     )
-    protected int renderHudWithShadow(TextRenderer textRenderer, MatrixStack matrices, String text, float x, float y, int color) {
+    private void renderLeftText(MatrixStack matrices, CallbackInfo ci, List<String> list, int i, String string, int j, int k, int l, int m) {
         if (BoringTweaksConfigPojo.featuresGroup.shadowedDebugHud) {
-            return textRenderer.drawWithShadow(matrices, text, x, y, color);
-        } else {
-            return textRenderer.draw(matrices, text, x, y, color);
+            String id = ci.getId();
+            if (id.equals("renderLeftText")) {
+                this.fontRenderer.drawWithShadow(matrices, string, 2.0F, (float)m, 14737632);
+            } else {
+                this.fontRenderer.drawWithShadow(matrices, string, (float)l, (float)m, 14737632);
+            }
         }
     }
 }
